c5e392b953174c119c293fdba6653b820bf769cf
Aryan <chaudharyaryan3456@gmail.com>
2024-12-23 14:49:44 +0530

    updated

---
+++ app.js
@@ -0,0 +1,6 @@
+// DOM(Document Object Model) --> The DOM represents a document with a logical tree.It allows us to manipulate/change WebPage content (HTML Elements).  
+// (1). Selecting Elements 
+// --> (i). getElementById = Returns the element as an object or null (if not found).
+// for Ex: document.getElementById('mainImg');
+// --> (ii). getElementByClassName = Returns the element as an object or null (if not found).
+// for Ex: document.getElementById('mainImg');
