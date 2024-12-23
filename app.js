024feed57870895151f42fb2f640f40a03e49d53
Aryan <chaudharyaryan3456@gmail.com>
2024-12-23 14:55:34 +0530

    updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // --> (i). getElementById = Returns the element as an object or null (if not found).
 // for Ex: document.getElementById('mainImg');
 // --> (ii). getElementByClassName = Returns the element as an HTML Collection or null (if not found).
-let smallImage = document.getElementsByClassName('oldimages');
+let smallImage = document.getElementsByClassName('oldImg');
 for(let i=0; i<smallImage.length; i++){
-    console.log(smallImage[i]);
+    console.dir(smallImage[i].src);
 }
