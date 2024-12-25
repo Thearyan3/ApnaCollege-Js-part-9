61b6d5621acb1bd4da7e6a8b0a69897ad8a4d550
Aryan <chaudharyaryan3456@gmail.com>
2024-12-25 23:51:55 +0530

    updated

--- app.js
+++ app.js
@@ -3,7 +3,14 @@
 // --> (i). getElementById = Returns the element as an object or null (if not found).
 // for Ex: document.getElementById('mainImg');
 // --> (ii). getElementByClassName = Returns the element as an HTML Collection or null (if not found).
+// 1). To print the image source on the console dir. 
 // let smallImage = document.getElementsByClassName('oldImg');
 // for(let i=0; i<smallImage.length; i++){
 //     console.dir(smallImage[i].src);
 // }
+// 2). To change the image source of the web page and log it on the console.
+let smallImage = document.getElementsByClassName('oldImg');
+for(let i=0; i<smallImage.length; i++){
+    smallImage[i].src = "creation_1.png";
+    console.log(`value of image no. ${i} is changed`);
+} 
