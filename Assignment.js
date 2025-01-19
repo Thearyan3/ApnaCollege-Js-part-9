42f26a1b20ab5dcab69890a92026cda9b155b3f7
Aryan <chaudharyaryan3456@gmail.com>
2025-01-19 23:38:17 +0530

    updated

--- Assignment.js
+++ Assignment.js
@@ -2,10 +2,32 @@
 let btn = document.createElement('button');
 btn.innerText = "Click Me";
 document.querySelector('body').append(btn);
+let input1 = document.createElement('input');
+input1.placeholder = "Placeholder";
+document.querySelector('body').append(input1);
+
+
+// Qs 2. - Add following attributes to the element :-
+// - Change placeholder value of input to “username”
+// - Change the id of button to “btn”.
+btn.setAttribute('id', 'btn');
+input1.setAttribute('placeholder', 'username');
 
 
 // Qs 1. - Create a new input and button element on the page using JavaScript only. Set the text of button as "Click Me".
-// Qs 1. - Create a new input and button element on the page using JavaScript only. Set the text of button as "Click Me".
-// Qs 1. - Create a new input and button element on the page using JavaScript only. Set the text of button as "Click Me".
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Qs 1. - Create a new input and button element on the page using JavaScript only. Set the text of button as "Click Me".
 // Qs 1. - Create a new input and button element on the page using JavaScript only. Set the text of button as "Click Me".
