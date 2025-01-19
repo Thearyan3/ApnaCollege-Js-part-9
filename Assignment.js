64d095659d89e65465164cf4bbc3d59c728017cb
Aryan <chaudharyaryan3456@gmail.com>
2025-01-19 23:33:36 +0530

    updated

---
+++ Assignment.js
@@ -0,0 +1,11 @@
+// Qs 1. - Create a new input and button element on the page using JavaScript only. Set the text of button as "Click Me".
+let btn = document.createElement('button');
+btn.innerText = "Click Me";
+document.querySelector('body').append(btn);
+
+
+// Qs 1. - Create a new input and button element on the page using JavaScript only. Set the text of button as "Click Me".
+// Qs 1. - Create a new input and button element on the page using JavaScript only. Set the text of button as "Click Me".
+// Qs 1. - Create a new input and button element on the page using JavaScript only. Set the text of button as "Click Me".
+// Qs 1. - Create a new input and button element on the page using JavaScript only. Set the text of button as "Click Me".
+// Qs 1. - Create a new input and button element on the page using JavaScript only. Set the text of button as "Click Me".
